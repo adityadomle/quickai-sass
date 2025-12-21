0ebfa064aa10d83bdf8a5d782f779be9843ff0eb
Aditya Domle <adityadomle14@gmail.com>
2025-12-21 19:16:29 +0530

    initial setup

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 import express from "express"
-import cors from "cors";
-import 'dotenv/config';
+import cors from "cors"
+import "dotenv/config"
 
 const app = express()
 
-app.use = (cors())
+app.use(cors())          // 👈 FIX
 app.use(express.json())
 
-app.get('/', (req, res)=>res.send('Server is Live!'))
+app.get("/", (req, res) => {
+  res.send("Server is Live!")
+})
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()=>{
-    console.log('Server is running on port', PORT);
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT)
 })
