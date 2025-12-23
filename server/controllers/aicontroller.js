c9ab961998b507d3aa6972c4c92e092006d6db3d
Aditya Domle <adityadomle14@gmail.com>
2025-12-23 23:14:26 +0530

    minor fix

--- server/controllers/aicontroller.js
+++ server/controllers/aicontroller.js
@@ -4,12 +4,14 @@ export const generateArticle = async (req, res) => {
     try {
         const { userId } = req.auth();
         const { prompt, length } = req.body;
-        const plan = req.plan;
+        const plan = req.plan; 
         const free_usage = req.free_usage
 
         if(plan !== 'premium' && free_usage >= 10){
             return res.json({ success: false, message: "Limit reached. Upgrade to continue."})
         }
+
+
     } catch (error) {
         
     }
