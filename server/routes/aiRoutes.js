5c53ce11ba4714fd028f0af80e1120c9485a625d
Aditya Domle <adityadomle14@gmail.com>
2025-12-24 23:59:23 +0530

    minor fix

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -6,6 +6,6 @@ const aiRouter = express.Router();
 
 aiRouter.post("/generate-article", auth, generateArticle);
 aiRouter.post("/generate-blog-title", auth, generateBlogTitle);
-aiRouter.post("/generate-image", auth, generateImage);
+aiRouter.post("/generate-image", auth, generateImage); 
 
 export default aiRouter; 
