933fb2818ddf113ac592b83d2fc0b871c0521eb4
Aditya Domle <adityadomle14@gmail.com>
2025-12-26 23:57:43 +0530

    minor changes

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import type { Project } from "../types"
+import { Loader2Icon } from "lucide-react"
 
 const MyProjects = () => {
   const [loading, setLoading] = useState(true)
@@ -9,15 +10,26 @@ const MyProjects = () => {
     // fetch logic here
   }
 
-  useEffect(() => {
-    fetchProjects()
-  }, [])
+ useEffect(() => {
+  fetchProjects()
+}, [])
 
-  return (
-    <div>
-      
+return (
+  <>
+    <div className="px-4 md:px-16 lg:px-24 xl:px-32">
+      {loading ? (
+        <div className="flex items-center justify-center h-[80vh]">
+          <Loader2Icon className="size-7 animate-spin text-indigo-200" />
+        </div>
+      ) : projects.length > 0 ? (
+        <div></div>
+      ) : (
+        <div></div>
+      )}
     </div>
-  )
+  </>
+)
+
 }
 
 export default MyProjects
